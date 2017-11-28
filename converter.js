f2c28bb519c33eef6ab8872755d226778df8fe0d
Melissa Bell <melissabell456@gmail.com>
2017-11-27 19:28:00 -0600

    reworked HTML and JS formulas. attempt 2 getting closer? targeting different inputs by ID

--- converter.js
+++ converter.js
@@ -1,29 +1,31 @@
-//     // Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
-    // Assign a function to be executed when the button is clicked
+
+// Get a reference to the button element in the DOM
+let button = document.getElementById("submitValue");
+
+// To target text field and get value of user entry
+let locateTempInput = document.getElementById("tempInput");
+let tempInput = parseInt(locateTempInput.value);
+console.log('This is the entered temp to convert', tempInput);
+
+// To target radio buttons
+let convertToF = document.getElementById("tempTypeF");
+let convertToC = document.getElementById("tempTypeC");
+
+
+// Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
 
-function determineConverter() {    
-    var userTemp = parseInt(document.getElementById("tempInput"))
-    console.log(userTemp);
-    if (document.getElementById("determineConverter") === Fahrenheit) {
-        var celciusToFahrenheit = userTemp * 1.8 + 32;
-        console.log(celciusToFahrenheit);    
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+
+function determineConverter (clickEvent) {
+    let convertedTemp;
+    if (convertToF.checked) {
+        convertedTemp = tempInput * 1.8 + 32 
+        console.log(convertedTemp);
     }
-    else (document.getElementById("determineConverter") === Celcius) {
-        var fahrenheitToCelsius = (userTemp - 32) * .5556;  
-        console.log(fahrenheitToCelsius);  
-    };
-
-
-    // if radio button was Fahrenheit
-function toFahrenheit () {
-    var celciusToFahrenheit = userTemp * 1.8 + 32
-    
-}
-// if radio button was Celcius
-function toCelsius () {
-    var fahrenheitToCelsius = (userTemp - 32) * .5556    
-}
-
-    
+    else if (convertToC.checked) {
+        convertedTemp = (tempInput - 32) * .5556   
+        console.log(convertedTemp); 
+    }
+};
